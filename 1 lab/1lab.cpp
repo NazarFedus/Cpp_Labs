7947299e89d45abda8f1018d159796fd61350233
NazarFedus <simpleprograming7@gmail.com>
2023-03-05 11:30:08 +0000

    lab1 is done

--- 1 lab/1lab.cpp
+++ 1 lab/1lab.cpp
@@ -1,95 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <iostream>
-using namespace std;
 
-#define N 100000
-#define REPEAT (count, code) for(int i = 0; i < count; i++){ code }
+#define DATA_SIZE 32
+#define REPEAT_COUNT 1000000
 
-double getCurrentTime(){
-     clock_t time = clock();
-     if(time != (clock_t) - 1){
-          return ((double)time / (double)CLOCKS_PER_SEC);
-     }
-     return 0;
+// Macro for repeat code
+#define REPEATOR(count, code) \
+for (unsigned int indexIteration = (count); indexIteration--;){ code; }
+double getCurrentTime() {
+	clock_t time = clock();
+	if (time != (clock_t)-1) {
+		return ((double)time / (double)CLOCKS_PER_SEC);
+	}
+	return 0.; // else
+}
+
+// Function to output array
+void printVector(void* data, int count/* 0 - full DATA_SIZE*/) {
+	int index = 0;
+	for (index = 0; (!count || index < count) && index < DATA_SIZE; index++) {
+		printf("%d ", ((int*)data)[index]);
+	}
+	printf("\n");
+}
+
+//QuickSort
+void quickSort(int* array, int leftIndex, int rightIndex) {
+	int iIndex = leftIndex;
+	int jIndex = rightIndex;
+	int xElemntValue = array[(leftIndex + rightIndex) / 2];
+	do {
+		while ((array[iIndex] < xElemntValue) && (iIndex < rightIndex)) {
+			++iIndex;
+		}
+		while ((xElemntValue < array[jIndex]) && (jIndex > leftIndex)) {
+			--jIndex;
+		}
+		if (iIndex <= jIndex) {
+			int temp = array[iIndex];
+			array[iIndex] = array[jIndex];
+			array[jIndex] = temp;
+			++iIndex;
+			--jIndex;
+		}
+	} while (iIndex <= jIndex);
+	if (leftIndex < jIndex) {
+		quickSort(array, leftIndex, jIndex);
+	}
+	if (iIndex < rightIndex) {
+		quickSort(array, iIndex, rightIndex);
+	}
+}
+
+//ShellSort
+void shellSort(int* array, int leftIndex, int rightIndex){
+
+	int dIndex, iIndex, jIndex;
+	int sortingSize = rightIndex - leftIndex + 1;
+	for (dIndex = leftIndex + sortingSize / 2; dIndex >= leftIndex + 1; dIndex /= 2)
+	{
+		for (iIndex = dIndex; iIndex < sortingSize; iIndex++)
+		{
+			for (jIndex = iIndex; jIndex >= dIndex && array[jIndex - dIndex] >
+				array[jIndex]; jIndex -= dIndex) {
+				int temp = array[jIndex];
+				array[jIndex] = array[jIndex - dIndex];
+				array[jIndex - dIndex] = temp;
+			}
+		}
+	}
 }
 
 
-// ShellSort
-void ShellSort(int n, int mass[])
-{
-    int i, j, step;
-    int tmp;
-    for (step = n / 2; step > 0; step /= 2)
-        for (i = step; i < n; i++)
-        {
-            tmp = mass[i];
-            for (j = i; j >= step; j -= step)
-            {
-                if (tmp < mass[j - step])
-                    mass[j] = mass[j - step];
-                else
-                    break;
-            }
-            mass[j] = tmp;
-        }
-}
+int main(void){
+     // begin array for quickSort
+	int data[DATA_SIZE] = { 11, 2, 29, 28, 10, 26, 25, 24,
+		23, 24, 21, 30, 19, 18, 7, 16,
+		17, 14, 13, 12, 10, 27, 9, 8,
+		7, 6, 5, 4, 3, 1, 0, 31 };
+	double startTime, endTime;
 
-// QuickSort
-void QuickSort (int *arr, int a, int b){
+	startTime = getCurrentTime();
+	REPEATOR(REPEAT_COUNT, quickSort(data, 0, DATA_SIZE-1));
+	endTime = getCurrentTime();
 
-     int i=a, j=b, m = rand ()% (b-a) +a;
-     int x = * (arr+m);
+     // output result
+	printf("QuickSort (run time): %dns\r\n\r", (unsigned int)((endTime - startTime) * (double)(1000000000 / REPEAT_COUNT)));
 
-     do{
-          while (i<=b && * (arr+i) < x) i++;
-          while (j>=a && * (arr+j) > x) j--;
+     printf("Sorted array: ");
+	printVector(data, 0);
 
-          if (i <= j){
-               if (i < j){
-                    int buf=* (arr+i);
-                    * (arr+i) =* (arr+j);
-                    * (arr+j) =buf;
-               }
-               i++;
-               j--;
-          }
+     // begin array for shellSort
+	int data1[DATA_SIZE] = { 11, 2, 29, 28, 10, 26, 25, 24,
+		23, 24, 21, 30, 19, 18, 7, 16,
+		17, 14, 13, 12, 10, 27, 9, 8,
+		7, 6, 5, 4, 3, 1, 0, 31 };
 
-     } while (i <= j);
+	startTime = getCurrentTime();
+	REPEATOR(REPEAT_COUNT, shellSort(data1, 0, DATA_SIZE - 1));
+	endTime = getCurrentTime();
 
-     if (i < b) QuickSort (arr, i,b);
+     // output result
+	printf("\nShellSort (run time): %dns\r\n\r", (unsigned int)((endTime - startTime) * (double)(1000000000 / REPEAT_COUNT)));
 
-     if (a < j) QuickSort (arr,a,j);
-}
+     printf("Sorted array: ");
+     printVector(data1, 0);
 
-int main(){
-
-     double startTime, endTime;
-
-     // For ShellSort
-          // input data
-          int mass[N];
-          for(int i = 0; i < N; i++){
-               mass[i] = rand() % 100;
-          }
-          // sort
-          startTime = getCurrentTime();
-          ShellSort(N, mass);
-          endTime = getCurrentTime();
-
-          cout << "ShellSort: " << endTime - startTime << endl;
-
-     // For QuickSort
-          // input data
-          int arr[N];
-          for(int i = 0; i < N; i++){
-               arr[i] = rand() % 100;
-          }
-          // sort
-          startTime = getCurrentTime();
-          QuickSort(arr, 0, N-1);
-          endTime = getCurrentTime();
-
-          cout << "QuickSort: " << endTime - startTime << endl;
+	return 0;
 }
